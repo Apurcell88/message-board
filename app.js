072a66b49b8a706376d6ceb12ba328dbae756ed3
Adam Purcell <apurcell88@gmail.com>
2024-12-27 13:00:37 -0600

    updated port.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-const PORT = 3000;
+const PORT = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", indexRouter);
